874d4610d1dea1550dc23dc067acf07de0131a4d
acunier@openpass.com.ar <acunier@openpass.com.ar>
2024-04-08 23:09:20 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,636 @@
+<!doctype html>
+<html lang="en">
+
+
+
+<head>
+
+    <!--====== Required meta tags ======-->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="Seguridad, Montevideo , Camaras ,Cercos">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--====== Title ======-->
+    <title>Seguridad empresarial &#8211; Video vigilancia en Alta definición</title>
+
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/png">
+
+    <!--====== Bootstrap css ======-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <!--====== Line Icons css ======-->
+ <link href="https://cdn.lineicons.com/3.0/lineicons.css" rel="stylesheet">
+
+
+    <!--====== Line Icons css ======-->
+ <link rel="stylesheet" href="assets/css/lineicons.css">
+    <!--====== Magnific Popup css ======-->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+    <!--====== Default css ======-->
+    <link rel="stylesheet" href="assets/css/default.css">
+
+    <!--====== Style css ======-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+      <!--====== Stylo del Boton Whatsapp ======-->
+    <link rel="stylesheet" href="assets/css/styleboton.css">
+
+
+  <!--====== Fuente css ======-->
+  
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+
+
+
+
+</head>
+
+<body>
+
+
+
+
+
+    <!--====== PRELOADER PART START ======-->
+
+    <div class="preloader">
+        <div class="loader_34">
+            <div class="ytp-spinner">
+                <div class="ytp-spinner-container">
+                    <div class="ytp-spinner-rotator">
+                        <div class="ytp-spinner-left">
+                            <div class="ytp-spinner-circle"></div>
+                        </div>
+                        <div class="ytp-spinner-right">
+                            <div class="ytp-spinner-circle"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--====== PRELOADER ENDS START ======-->
+
+    <!--====== HEADER PART START ======-->
+
+    <header id="home" class="header-area">
+        <div class="navigation fixed-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <nav class="navbar navbar-expand-lg">
+                            <a class="navbar-brand" href="index.html">
+                                <img src="assets/images/logo.png" alt="Logo" style="max-width:200px;">
+                            </a> <!-- Logo -->
+                          
+
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                
+                            </div> <!-- navbar collapse -->
+                        </nav> <!-- navbar -->
+                    </div>
+                </div> <!-- row -->
+            </div> <!-- container -->
+        </div> <!-- navigation -->
+
+
+
+
+
+        
+    </header>
+
+    <!--====== HEADER PART ENDS ======-->
+
+
+
+ <!--====== ABOUT PART START ======-->
+
+ <section id="about" class="about-area pt-125 pb-130">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="section-title text-center">
+                        <h2 class="title">CAMARAS DE SEGURIDAD</h2>
+                        <h4 style="color:black;">Somos instaladores oficiales de la línea Hikvision, marca líder en el rubro de seguridad.</h4>
+                    </div> <!-- section title -->
+                </div>
+            </div>
+            
+            <!-- row -->
+            <div class="row">
+               
+                <div class="col-lg-6">
+                    <div class="about-content mt-50">
+                      <img src="assets/images/POST-CAMARAS-DE-SEGURIDAD-1024x1024.jpg">  
+                    </div> <!-- about content -->
+                
+                </div>
+
+                <div class="col-lg-6">
+                    <div class="about-content mt-50">
+                    </br>
+                    </br>
+                    </br>
+                        <h3 class="about-title" style="color: black;">Nuestros sistemas incluyen:</br>
+
+*Imágenes coloridas aun en la noche</br>
+
+*Detección inteligente en caso de cruces de línea por humanos o vehículos</br>
+
+*Aviso de alarmas mediante app y monitoreo de las cámaras remotamente</br>
+
+ 
+
+Consulte por instalaciones desde cero o actualización de su sistema, sacando provecho de su inversión anterior.</br>
+</br>
+Solicite un asesor sin costo, mediante formulario o al </br>097 60 60 60</h3>
+
+                        
+
+                     
+                        
+                    </div> <!-- about content -->
+                
+                </div>
+
+
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+
+    <!--====== ABOUT PART ENDS ======-->
+
+  <!--====== CONTACT PART START ======-->
+
+    <section id="contact" class="contact-area pt-125 pb-130 gray-bg">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="section-title text-center pb-25">
+                        <h2 class="title"></h2>
+                        <h4 style="color:black;">SOLICITAR UN ASESOR SIN CARGO</h4>
+                    </div> <!-- section title -->
+                </div>
+            </div> 
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="contact-form pt-30">
+                        <form id="contact-form" action="contacto.php" method="post">
+                            <div class="single-form">
+                                <input type="text" name="name" placeholder="Nombre">
+                            </div> <!-- single form -->
+                            <div class="single-form">
+                                <input type="email" name="email" placeholder="Email">
+                            </div> <!-- single form -->
+                            <div class="single-form">
+                                <textarea name="message" placeholder="Mensaje"></textarea>
+                            </div> <!-- single form -->
+                            <p class="form-message"></p>
+                            <div class="single-form">
+                                <button class="main-btn" type="submit" style="border-color: #bea925;color:white;">Enviar Mensaje</button>
+                            </div> <!-- single form -->
+                        </form>
+                    </div> <!-- contact form -->
+                </div>
+                <div class="col-lg-6">
+                    <div class="contact-map mt-60">
+                        <div class="gmap_canvas"> 
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d204.4610541203434!2d-57.9602298!3d-34.9220842!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2e62d6065ce1b%3A0xb0d422b374666c16!2sCalle%2048%201044%2C%20B1900ANT%20La%20Plata%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1627505506129!5m2!1ses-419!2sar" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                        
+
+
+
+
+                        </div>
+                    </div> <!-- contact map -->
+                </div>
+            </div> <!-- row -->
+
+            <!-- row -->
+            <div class="row justify-content-center">
+                <div class="col-lg-4 col-md-6 col-sm-7">
+                    <div class="contact-box text-center mt-30">
+                        <div class="contact-icon">
+                            <i class="lni lni-postcard"></i>
+                        </div>
+                        <div class="contact-content">
+                            <h6 class="contact-title">Dirección</h6>
+                            <p>SANTA FE 1325, MONTEVIDEO </p>
+                        </div>
+                    </div> <!-- contact box -->
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-7">
+                    <div class="contact-box text-center mt-30">
+                        <div class="contact-icon">
+                            <i class="lni lni-phone"></i>
+                        </div>
+                        <div class="contact-content">
+                            <h6 class="contact-title">Telefono</h6>
+                            <p>+598 97 60 60 60</p>
+                        </div>
+                    </div> <!-- contact box -->
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-7">
+                    <div class="contact-box text-center mt-30">
+                        <div class="contact-icon">
+                            <i class="lni lni-envelope"></i>
+                        </div>
+                        <div class="contact-content">
+                            <h6 class="contact-title">Email</h6>
+                            <p>info@royalsecurity.com.uy</p>
+
+                        </div>
+                    </div> <!-- contact box -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+
+   
+
+<section id="team" class="team-area pt-120">
+<div class="container">
+<div class="row justify-content-center">
+<div class="col-lg-8">
+<div class="section-title text-center pb-30">
+    <div class="line m-auto"></div>
+    <h2 class="title">CONFIAN EN NOSOTROS</h2>
+    <h3 style="color:#60817a;">Siempre listos para atenderlos</h3>
+    </div> 
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="col-lg-12 col-md-7 col-sm-8">
+<div class="single-team text-center mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.8s">
+<div class="team-image">
+<div class="social">
+</div>
+</div>
+<div class="team-content">
+<h5 class="holder-name" >Nutrici&oacute;n 
+</h5>
+<p class="text">Lic.Rabe Ailen
+ 
+ </br>Especialista en Nutrici&oacute;n
+
+</br> MP 5724</p>
+</div>
+</div> 
+</div>
+ 
+<div class="row justify-content-center">
+<div class="col-lg-4 col-md-7 col-sm-8">
+<div class="single-team text-center mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.8s">
+<div class="team-image">
+<div class="social">
+</div>
+</div>
+<div class="team-content">
+<h5 class="holder-name" >Pediatr&iacute;a </h5>
+<p class="text">Dra. Curzel María Cecilia 
+ 
+ </br>Especialista en Pediatría y Neonatología 
+
+</br>MP 117690 </p>
+</div>
+</div> 
+</div>
+
+
+
+
+
+
+
+
+
+<div class="col-lg-4 col-md-7 col-sm-8">
+<div class="single-team text-center mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.2s">
+<div class="team-image">
+<div class="social">
+</div>
+</div>
+<div class="team-content">
+<h5 class="holder-name" >Cl&iacute;nica M&eacute;dica</h5>
+<p class="text">Dra.Giudice Marina
+ 
+ </br>Especialista Jerarquizada  en Dermatolog&iacute;a
+ </br> MP112884</p>
+
+</div>
+</div> 
+</div>
+<div class="col-lg-4 col-md-7 col-sm-8">
+<div class="single-team text-center mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
+<div class="team-image">
+
+<div class="social">
+
+</div>
+</div>
+<div class="team-content">
+<h5 class="holder-name" >Pediatr&iacute;a </h5>
+<p class="text">Dra. María Ángeles Azrak 
+ 
+ </br> Médica consultor en Pediatría.</br>MP 111473 </p>
+</div>
+</div> 
+</div>
+<div class="col-lg-4 col-md-7 col-sm-8">
+<div class="single-team text-center mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.8s">
+<div class="team-image">
+<div class="social">
+</div>
+</div>
+<div class="team-content">
+<h5 class="holder-name" >Cl&iacute;nica M&eacute;dica </h5>
+<p class="text">Dr. Gioiosa Marcelo A. 
+ 
+ </br>Especialista en Cl&iacute;nica M&eacute;dica
+
+</br>MP 117491 </p>
+</div>
+</div> 
+</div>
+
+<div class="col-lg-4 col-md-7 col-sm-8">
+<div class="single-team text-center mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.8s">
+<div class="team-image">
+<div class="social">
+</div>
+</div>
+<div class="team-content">
+<h5 class="holder-name" >Pediatr&iacute;a </h5>
+<p class="text">Dra. Picone Maria Florencia 
+
+
+ </br>Especialista Jerarquizada en Pediatría y Neonatología
+
+</br>MP 111508 </p>
+</div>
+</div> 
+</div>
+
+
+<div class="col-lg-4 col-md-7 col-sm-8">
+<div class="single-team text-center mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.8s">
+<div class="team-image">
+<div class="social">
+</div>
+</div>
+<div class="team-content">
+<h5 class="holder-name" >Cl&iacute;nica M&eacute;dica </h5>
+<p class="text">Dra. Rosselli Julieta
+
+
+ </br>Especialista Jerarquizada  en Cl&iacute;nica M&eacute;dica
+
+</br>MP 113445 </p>
+</div>
+</div> 
+</div>
+
+<div class="col-lg-4 col-md-7 col-sm-8">
+<div class="single-team text-center mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.8s">
+<div class="team-image">
+<div class="social">
+</div>
+</div>
+<div class="team-content">
+<h5 class="holder-name" >Infectolog&iacute;a  </h5>
+<p class="text">Dra.Anabella Greco
+ 
+ </br>Especialista en Infectolog&iacute;a
+
+</br> MP231357</p>
+</div>
+</div> 
+</div>
+
+
+<div class="col-lg-4 col-md-7 col-sm-8">
+<div class="single-team text-center mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.8s">
+<div class="team-image">
+<div class="social">
+</div>
+</div>
+<div class="team-content">
+<h5 class="holder-name" >Dermatolog&iacute;a  </h5>
+<p class="text">Dra.Mar&iacute;a Agustina Sicre
+ 
+ </br>Dermatolog&iacute;a
+
+</br> MP119370</p>
+</div>
+</div> 
+</div>
+
+
+
+<div class="col-lg-4 col-md-7 col-sm-8">
+<div class="single-team text-center mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.8s">
+<div class="team-image">
+<div class="social">
+</div>
+</div>
+<div class="team-content">
+<h5 class="holder-name" >Cl&iacute;nica M&eacute;dica
+</h5>
+<p class="text">Dra.Aguilera Roxana
+ 
+ </br>Especialista   en Cl&iacute;nica M&eacute;dica
+
+</br> MP117292</p>
+</div>
+</div> 
+</div>
+
+
+
+
+
+
+
+
+
+
+</div> 
+</div> 
+</section>
+
+</br>
+
+
+   
+  
+        
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<a href="https://api.whatsapp.com/send?phone=59897006060&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n" class="float" target="_blank" >
+<i class="fa fa-whatsapp my-float" style="color:white;font-size:30px;"></i>
+</a>
+
+
+    <!--====== CONTACT PART ENDS ======-->
+
+    <!--====== FOOTER PART START ======-->
+
+    <footer id="footer" class="footer-area">
+        <div class="footer-widget pt-130 pb-130">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="footer-content text-center">
+                            <a href="index.html">
+                                <img src="assets/images/logo-2.png" alt="Logo">
+                            </a>
+                            <p class="mt-">SEGURIDAD EMPRESARIAL</p>
+                            <ul>
+                                <li><a href="https://www.facebook.com/cemec.laplata"><i class="lni lni-facebook-filled"></i></a></li>
+                                <li><a href="https://www.instagram.com/cemec.laplata/#"><i class="lni lni-instagram"></i></a></li>
+                                <li><a href="#"><i class="lni lni-linkedin-original"></i></a></li>
+
+
+
+
+
+
+                            </ul>
+                        </div> <!-- footer content -->
+                    </div>
+                </div> <!-- row -->
+            </div> <!-- container -->
+        </div> <!-- footer widget -->
+        <div class="footer-copyright pb-20">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="copyright-text text-center pt-20">
+                            <p>Copyright © 2022. Template Crafted by <a href="https://uideck.com" rel="nofollow">UIdeck</a></p>
+                        </div> <!-- copyright text -->
+                    </div>
+                </div> <!-- row -->
+            </div> <!-- container -->
+        </div> <!-- footer widget -->
+    </footer>
+
+    <!--====== FOOTER PART ENDS ======-->
+
+    <!--====== BACK TOP TOP PART START ======-->
+
+   <!-- <a href="#" class="back-to-top"><i class="lni-chevron-up"></i></a> -->
+
+    <!--====== BACK TOP TOP PART ENDS ======-->
+
+
+
+
+
+
+
+    <!--====== jquery js ======-->
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+
+    <!--====== Bootstrap js ======-->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+
+    <!--====== Magnific Popup js ======-->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+
+    <!--====== Parallax js ======-->
+    <script src="assets/js/parallax.min.js"></script>
+
+    <!--====== Counter Up js ======-->
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+
+
+    <!--====== Appear js ======-->
+    <script src="assets/js/jquery.appear.min.js"></script>
+
+    <!--====== Scrolling js ======-->
+    <script src="assets/js/scrolling-nav.js"></script>
+    <script src="assets/js/jquery.easing.min.js"></script>
+
+
+    <!--====== Main js ======-->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+
+
+<?php
+/**
+ * @version 1.0
+ */
+
+
+// Valores enviados desde el formulario
+
+    
+
+
+
+// Datos de la cuenta de correo utilizada para enviar vía SMTP
+$smtpHost = "c2451219.ferozo.com";  // Dominio alternativo brindado en el email de alta 
+$smtpUsuario = "no-reply@update.com.uy";  // Mi cuenta de correo
+$smtpClave = "f5*YKRk0HB";  // Mi contraseña
+
+// Email donde se enviaran los datos cargados en el formulario de contacto
+$emailDestino = "afrutos@gmail.com";
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Port = 465; 
+$mail->SMTPSecure = 'ssl';
+$mail->IsHTML(true); 
+$mail->CharSet = "utf-8";
+
+
+// VALORES A MODIFICAR //
+$mail->Host = $smtpHost; 
+$mail->Username = $smtpUsuario; 
+$mail->Password = $smtpClave;
+
+$mail->From = $email; // Email desde donde envío el correo.
+$mail->FromName = $nombre;
+$mail->AddAddress($emailDestino); // Esta es la dirección a donde enviamos los datos del formulario
+
+$mail->Subject = "Contacto desde Pagina"; // Este es el titulo del email.
+$mensajeHtml = nl2br($mensaje);
+$mail->Body = "{$mensajeHtml} <br /><br />Contacto desde Pagina <br />"; // Texto del email en formato HTML
+$mail->AltBody = "{$mensaje} \n\n UPDATE"; // Texto sin formato HTML
+// FIN - VALORES A MODIFICAR //
+
+$estadoEnvio = $mail->Send(); 
+
+
+?>
+
+
+
+
+</html>
